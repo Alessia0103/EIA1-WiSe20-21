aab82b8cf62c79d96a796a2affa63bd231a1c332
Alessia0103 <72872801+Alessia0103@users.noreply.github.com>
2020-11-17 16:00:29 +0100

    Aufgabe 5

---
+++ Aufgabe_5/script.js
@@ -0,0 +1,42 @@
+console.log("hey");
+var europe08 = 4965.7;
+var europe18 = 4209.3;
+console.log("Europe produces "+ europe18 + "kg CO2");
+
+var africa08 = 1028;
+var africa18 = 1235.5;
+console.log("Africa produces "+ africa18 + "kg CO2");
+
+var southa08 = 1132.6;
+var southa18 = 1261.5;
+console.log("South America produces "+ southa18 + "kg CO2");
+
+var northa08 = 6600.4;
+var northa18 = 6035.6;
+console.log("North America produces "+ northa18 + "kg CO2");
+
+var asia08 = 12954.7;
+var asia18 = 16274.1;
+console.log("Asia produces "+ asia18 + "kg CO2");
+
+var total = europe18 + africa18 + southa18 + northa18 + asia18;
+
+console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit " + (europe18 / total * 100) + "%");
+console.log("Relativ zur Gesamtemission der Welt verursacht Africa damit " + (africa18 / total * 100) + "%");
+console.log("Relativ zur Gesamtemission der Welt verursacht South America damit " + (southa18 / total * 100) + "%");
+console.log("Relativ zur Gesamtemission der Welt verursacht North America damit " + (northa18 / total * 100) + "%");
+console.log("Relativ zur Gesamtemission der Welt verursacht Asia damit " + (asia18 / total * 100) + "%");
+
+console.log( "Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + ( europe18 / europe08 * 100) + "% verändert");
+console.log( "2018 im Vergleich zu 2008 sind das " + ( europe18 - europe08 ) + "kg CO2");
+console.log( "Für Africa hat sich 2018 im Vergleich zu 2008 die Emission um " + ( africa18 / africa08 * 100) + "% verändert");
+console.log( "2018 im Vergleich zu 2008 sind das " + ( africa18 - africa08 ) + "kg CO2");
+console.log( "Für South America hat sich 2018 im Vergleich zu 2008 die Emission um " + ( southa18 / southa08 * 100) + "% verändert");
+console.log( "2018 im Vergleich zu 2008 sind das " + ( southa18 - southa08 ) + "kg CO2");
+console.log( "Für North America hat sich 2018 im Vergleich zu 2008 die Emission um " + ( northa18 / northa08 * 100) + "% verändert");
+console.log( "2018 im Vergleich zu 2008 sind das " + ( northa18 - northa08 ) + "kg CO2");
+console.log( "Für Asia hat sich 2018 im Vergleich zu 2008 die Emission um " + ( asia18 / asia08 * 100) + "% verändert");
+console.log( "2018 im Vergleich zu 2008 sind das " + ( asia18 - asia08 ) + "kg CO2");
+
+
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_5/script.ts
@@ -0,0 +1,11 @@
+console.log("hallo welt");
+var total = 4209.3 + 2100.5 + 16275 + 1261.5 +
+    6035.6 + 1235.5;
+var countries = {
+    Europa: [4209.3, 4965.7],
+    Australien: [2100.5, 1993],
+    Asien: [16274.1, 12954.7],
+    "Süd Amerika": [1261.5, 1261.5],
+    "Nord Amerika": [6025.6, 6600.4],
+    Africa: [1235.5, 1028],
+};
